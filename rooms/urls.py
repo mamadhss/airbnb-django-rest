1a6e080232be5ee0e1c9006b39226e8bbc8788ac
mamadhss <mamadhss@yahoo.com>
2021-10-26 13:02:32 +0330

    test for api_view

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from . import views
 app_name = "rooms"
 
-urlpatterns = []
+urlpatterns = [
+    path("/",views.RootFunc),
+]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,19 @@
+from re import I
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+class RootAPIView(APIView):
+    def get(self,request):
+        return Response({
+            "Hello":"Django"
+        })
+
+
+@api_view(["GET"])
+def RootFunc(request):
+    return Response({
+        'test':'func'
+    })        
